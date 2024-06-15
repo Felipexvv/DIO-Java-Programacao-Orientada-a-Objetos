a62a943c03b10d23b2454230b9894b67c7c93273
Felipexvv <felipexvv@gmail.com>
2024-06-15 15:34:09 -0300

    Classes Curso e mentoria desafio POO dio

--- Desafio2-DIO-BootCamp/src/Main.java
+++ Desafio2-DIO-BootCamp/src/Main.java
@@ -1,5 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Engenharia de Java");
+        curso1.setDescricao("Aprendendo java e praticando");
+        curso1.setCargaHoraria(87);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Engenharia de software");
+        curso2.setDescricao("Aprendendo a projetar e gerenciar criacoes e desenvolvimento de projetos");
+        curso1.setCargaHoraria(3000);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Ensinando java");
+        mentoria.setData(LocalDate.now());
+
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
     }
 }
